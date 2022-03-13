d12064e7d293e821fae93715fff653db50d4c066
Ankit Ojha <ankitjh521@gmail.com>
2022-03-13 23:19:38 +0530

    routing: Link component routing examples added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 function Home() {
-    return <h1>Home Page</h1>
+    return <div>
+        <h1>Home Page</h1>
+        To navigate to Products. Click  
+        <Link href="/product">
+            <a>&nbsp;here</a>
+        </Link>
+    </div>
 }
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,3 +1,27 @@
-export default function Product() {
-    return <h1>Product Page</h1>
+import Link from "next/link";
+
+function Product({ productId = 55}) {
+    return (
+        <>
+            <h1>Product Page</h1>
+            To navigate to home page. Click
+            <Link href="/">
+                <a>&nbsp;here</a>
+            </Link>
+            <br />
+            <Link href="/product/1">
+                <a>&nbsp;Product 1</a>
+            </Link>
+            <br />
+            <Link href="/product/2" replace>
+                <a>&nbsp;Product 2</a>
+            </Link>
+            <br />
+            <Link href={`/product/${productId}`}>
+                <a>&nbsp;Product {productId}</a>
+            </Link>
+        </>
+    );
 }
+
+export default Product;
