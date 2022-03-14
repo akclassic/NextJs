b966271c4b5efc8039fb714b1e94ff980c5eea97
Ankit Ojha <ankitjh521@gmail.com>
2022-03-15 00:25:31 +0530

    routing: navigate programatically examples added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+
+    const router = useRouter();
+
+    const handleClick = () => {
+        console.log("Placing your order");
+        router.push("/product");
+    }
+
     return <div>
         <h1>Home Page</h1>
         To navigate to Products. Click  
         <Link href="/product">
             <a>&nbsp;here</a>
         </Link>
+        <br />
+        <button onClick={handleClick}>
+            Place Order
+        </button>
     </div>
 }
 
